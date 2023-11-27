3396fda119abce714c524e1b8f993b04b337a103
smilepay.id <150902692+Indonesia-SmilePay@users.noreply.github.com>
2023-11-27 18:23:18 +0800

    Update step1_generate_rsa_keys.js

--- step1_generate_rsa_keys.js
+++ step1_generate_rsa_keys.js
@@ -1,11 +1,11 @@
 const crypto = require('crypto');
 
-// 生成RSA密钥对
+// generate RSA pari
 const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
     modulusLength: 2048,
 });
 
-// 将私钥和公钥进行Base64编码并输出为字符串
+// Base64 -> string
 const encodedPrivateKey = Buffer.from(privateKey.export({ type: 'pkcs8', format: 'der' })).toString('base64');
 const encodedPublicKey = Buffer.from(publicKey.export({ type: 'spki', format: 'der' })).toString('base64');
 
